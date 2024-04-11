4fbaf7cb74f413fe6fbcf774716f969a1d2c41d5
Max Dodson <153779222+maxhdodson@users.noreply.github.com>
2024-04-11 18:46:13 -0500

    Create an array that filters out an imput

--- 04_removeFromArray/solution/removeFromArray-solution.js
+++ 04_removeFromArray/solution/removeFromArray-solution.js
@@ -1,4 +1,3 @@
-// we have 2 solutions here, an easier one and a more advanced one.
 // The easiest way to get an array of the rest of the arguments that are passed to a function
 // is using the rest operator. If this is unfamiliar to you look it up!
 const removeFromArray = function (array, ...args) {
@@ -13,16 +12,8 @@ const removeFromArray = function (array, ...args) {
       newArray.push(item);
     }
   });
-  // and return that array
   return newArray;
 };
 
-// A simpler, but more advanced way to do it is to use the 'filter' function,
-// which basically does what we did with the forEach above.
-
-// var removeFromArray = function(array, ...args) {
-//   return array.filter(val => !args.includes(val))
-// }
-//
 
 module.exports = removeFromArray;
