08c0b63b5268f05b29b69afec4025abc66f35356
Max Dodson <153779222+maxhdodson@users.noreply.github.com>
2024-04-15 17:57:03 -0500

    Create a celsius and fahrenheight converter

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(x) {
+  let fahrenheit = ((x -32) * 5/9);
+fahrenheit = (Math.round(fahrenheit * 10) / 10);
+
+  return fahrenheit;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(x) {
+  let celcius = ((x *9/5) + 32);
+  celcius = (Math.round(celcius * 10) / 10);
+
+  return celcius;
 };
 
 // Do not edit below this line
